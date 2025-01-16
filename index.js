2ed5c69560460adf65970102627b4c4e984a5cb1
matheus-calixto-silva <matheuscalixto8@gmail.com>
2025-01-16 14:45:37 -0300

    feat: 8-6 implement addBook mutation
    Implemented the addBook mutation that allows adding a new book. The mutation accepts title, author, publication year and genres as parameters. The mutation works even if the author is not previously saved on the server.

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const { ApolloServer } = require('@apollo/server');
 const { startStandaloneServer } = require('@apollo/server/standalone');
+const { v1: uuid } = require('uuid');
 
 let authors = [
   {
@@ -119,6 +120,15 @@ const typeDefs = `
     allBooks(author: String, genre: String): [Book!]!
     allAuthors: [Author!]!
   }
+
+  type Mutation {
+  addBook(
+    title: String!
+    published: Int!
+    author: String!
+    genres: [String!]!
+  ): Book
+  }
 `;
 
 const resolvers = {
@@ -150,6 +160,18 @@ const resolvers = {
         ).length;
         return { ...author, bookCount };
       });
+    },
+  },
+  Mutation: {
+    addBook: (_root, args) => {
+      const book = { ...args, id: uuid() };
+      books = books.concat(book);
+
+      if (!authors.find((author) => author.name === args.author)) {
+        authors = authors.concat({ name: args.author, id: uuid() });
+      }
+
+      return book;
     },
   },
 };
