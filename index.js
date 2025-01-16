2e3365f29f72cd4ac2a4a925e1232dbb62062569
matheus-calixto-silva <matheuscalixto8@gmail.com>
2025-01-16 14:16:23 -0300

    feat: 8-5 add optional genre parameter to allBooks query
    Modified the allBooks query to accept an optional genre parameter. The response will only include books of the specified genre.

--- index.js
+++ index.js
@@ -116,7 +116,7 @@ const typeDefs = `
   type Query {
     bookCount: Int!
     authorCount: Int!
-    allBooks(author: String): [Book!]!
+    allBooks(author: String, genre: String): [Book!]!
     allAuthors: [Author!]!
   }
 `;
@@ -126,11 +126,22 @@ const resolvers = {
     bookCount: () => books.length,
     authorCount: () => authors.length,
     allBooks: (_root, args) => {
-      if (!args.author) {
+      if (!args.author && !args.genre) {
         return books;
       }
 
-      return books.filter((book) => book.author === args.author);
+      if (args.author && !args.genre) {
+        return books.filter((book) => book.author === args.author);
+      }
+
+      if (!args.author && args.genre) {
+        return books.filter((book) => book.genres.includes(args.genre));
+      }
+
+      return books.filter(
+        (book) =>
+          book.author === args.author && book.genres.includes(args.genre)
+      );
     },
     allAuthors: (_root) => {
       return authors.map((author) => {
