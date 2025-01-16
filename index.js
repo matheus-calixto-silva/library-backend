e5f2cce0a0bcb17e4a119c6d2429977bc3f58a14
matheus-calixto-silva <matheuscalixto8@gmail.com>
2025-01-16 13:54:21 -0300

    feat: 8-4 add optional author parameter to allBooks query
    Modified the allBooks query to accept an optional author parameter. The response will only include books written by the specified author.

--- index.js
+++ index.js
@@ -116,7 +116,7 @@ const typeDefs = `
   type Query {
     bookCount: Int!
     authorCount: Int!
-    allBooks: [Book!]!
+    allBooks(author: String): [Book!]!
     allAuthors: [Author!]!
   }
 `;
@@ -125,7 +125,13 @@ const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
-    allBooks: () => books,
+    allBooks: (_root, args) => {
+      if (!args.author) {
+        return books;
+      }
+
+      return books.filter((book) => book.author === args.author);
+    },
     allAuthors: (_root) => {
       return authors.map((author) => {
         const bookCount = books.filter(
