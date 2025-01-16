c6be40de4eb02fdc07759d3b09964f70cfc36a2c
matheus-calixto-silva <matheuscalixto8@gmail.com>
2025-01-16 15:01:15 -0300

    feat: 8-7 implement editAuthor mutation
    Implemented the editAuthor mutation that allows you to set the year of birth of an author. The mutation accepts the author's name and year of birth as parameters.

--- index.js
+++ index.js
@@ -122,12 +122,13 @@ const typeDefs = `
   }
 
   type Mutation {
-  addBook(
-    title: String!
-    published: Int!
-    author: String!
-    genres: [String!]!
-  ): Book
+    addBook(
+      title: String!
+      published: Int!
+      author: String!
+      genres: [String!]!
+    ): Book
+    editAuthor(name: String!, setBornTo: Int!): Author
   }
 `;
 
@@ -172,6 +173,20 @@ const resolvers = {
       }
 
       return book;
+    },
+    editAuthor: (_root, args) => {
+      const author = authors.find((author) => author.name === args.name);
+
+      if (!author) {
+        return null;
+      }
+
+      const updatedAuthor = { ...author, born: args.setBornTo };
+      authors = authors.map((author) =>
+        author.name === args.name ? updatedAuthor : author
+      );
+
+      return updatedAuthor;
     },
   },
 };
